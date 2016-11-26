37c68b4ec5779f2661bb49574ab17733a9c5551d
Atif Niyaz <aniyaz@purdue.edu>
2016-11-26 17:20:14 -0500

    Added Homework 7 Structure

---
+++ hw/hw07.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int getFuelRange();
+int getStartingMileMarker();
+void getMileMarkerPositions(int []);
+
+int main() {
+
+  int fuelRange = getFuelRange();
+  int stratingMarker = getStartingMileMarker();
+  int mileMarkers [50] = {};
+
+  getMileMarkerPositions(mileMarkers);
+  return(0);
+}
+
+int getFuelRange() {
+  return(0);
+}
+
+int getStartingMileMarker() {
+  return(0);
+}
+
+void getMileMarkerPositions(int mileMarkers []) {
+
+}
